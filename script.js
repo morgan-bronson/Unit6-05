61bf5e1d0094a3d751819bf3014c6fcdff87a9fd
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-06 15:57:54 +0000

    comments

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ function time () {
   // gets day of week from html document
   if (weekday === 'Saturday' || weekday === 'Sunday') {
     alert('Time To Relax')
+    //makes an alert pop up
   } else if (age <= 18) {
     alert('Time For School')
+    // makes an alert pop up
   } else {
     alert('Time For Work')
+    // makes an alert pop up
   }
 }
