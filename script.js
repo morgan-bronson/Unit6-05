fc8b82e4d5c527e6aadd1d95acbc6ef326cc64f1
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-06 15:38:50 +0000

    changed values

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('mybtn').addEventListener('click', time)
 function time () {
   const age = (document.getElementById('age').value)
   // gets age from html document
-  const day = (document.getElementById('weekday').value)
+  const weekday = (document.getElementById('weekday').value)
   // gets day of week from html document
   if ((age >= 18) && (weekday >=4)) {
     alert('Time For School!')
